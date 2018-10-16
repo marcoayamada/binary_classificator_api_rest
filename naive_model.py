db481bfbdde7d5a0f45efdf050d609a688ef1cec
Marco <marcoayamada@gmail.com>
2018-10-16 12:58:29 -0300

    api done and working

--- naive_model.py
+++ naive_model.py
@@ -1,12 +1,20 @@
+# not should import
+#from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.naive_bayes import BernoulliNB
+
 from sklearn.externals import joblib
-from sklearn.feature_extraction.text import CountVectorizer
 import pickle
 import numpy as np
 
 classifier = joblib.load('./pickles/text_binary_clas.save')
 vectorizer = pickle.load(open(r'vectorizer/list_frequency.vec', 'rb'))
 
-def phrase_pred(phrase):
-    aaa = phrase.get("p1", None)
-    x = np.array([aaa])
-    return str(classifier.predict(vectorizer.transform(x)).tolist())
+def phrase_pred(body):
+	phrases = body.get('phrases', None)
+	preds = classifier.predict(vectorizer.transform(phrases)).tolist()
+
+	result={}
+	for a,b in zip(phrases, preds):
+		result[a] = b
+
+	return result
